e3418f87c43cfabb58f34dfbd5201b7db8c46c8a
Ricky Mao <rickyrmao@gmail.com>
2020-09-08 03:20:37 -0700

    Updates

---
+++ precache-manifest.9e77be9a5462994352aff880da041108.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c8980c1a6b3ee3aa8cdf985d4248a0",
+    "url": "/shoppies/index.html"
+  },
+  {
+    "revision": "f3c0c6223eaf616b460b",
+    "url": "/shoppies/static/css/main.57df7bb3.chunk.css"
+  },
+  {
+    "revision": "b5719e0cd40f42bb08ff",
+    "url": "/shoppies/static/js/2.a4b0d954.chunk.js"
+  },
+  {
+    "revision": "17dffe90612e048fd9b4a769ac11b9cb",
+    "url": "/shoppies/static/js/2.a4b0d954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3c0c6223eaf616b460b",
+    "url": "/shoppies/static/js/main.c3bdd632.chunk.js"
+  },
+  {
+    "revision": "27778a2488007f8c9380",
+    "url": "/shoppies/static/js/runtime-main.da667ea0.js"
+  }
+]);
